d39da1e3504af9ae8f11ad4cf14d43e56270194f
Peter Galonza <petewindows2011@yandex.ru>
2017-08-09 11:06:00 +0300

    fix Dockerfile for zabbix web

--- DockerPHP-FPM/zabbix.conf.php
+++ DockerPHP-FPM/zabbix.conf.php
@@ -3,8 +3,8 @@
 global $DB;
 
 $DB['TYPE']     = 'MYSQL';
-$DB['SERVER']   = '127.0.0.1';
-$DB['PORT']     = '3306';
+$DB['SERVER']   = 'mariadb-master';
+$DB['PORT']     = '0';
 $DB['DATABASE'] = 'zabbix';
 $DB['USER']     = 'zabbix';
 $DB['PASSWORD'] = '***REMOVED***';
@@ -12,8 +12,8 @@ $DB['PASSWORD'] = '***REMOVED***';
 // Schema name. Used for IBM DB2 and PostgreSQL.
 $DB['SCHEMA'] = '';
 
-$ZBX_SERVER      = 'localhost';
+$ZBX_SERVER      = '192.168.0.230';
 $ZBX_SERVER_PORT = '10051';
-$ZBX_SERVER_NAME = '';
+$ZBX_SERVER_NAME = 'Silvana-Server';
 
 $IMAGE_FORMAT_DEFAULT = IMAGE_FORMAT_PNG;
